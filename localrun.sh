7b21f98cc779165db97bbd929eff0ff80acf56e6
mjodorczuk <michaljanodorczuk@gmail.com>
2024-10-25 11:30:41 +0200

    Generate all the fields necessery for custom neutral field

--- localrun.sh
+++ localrun.sh
@@ -4,6 +4,8 @@ rm data/* 2> /dev/null
 # cp test_object_64_32_32.grid.h5 data/object.grid.h5
 python3 script/ObjectCreation/NorSat.py 32 32 32 10 5 0 data/object.grid.h5
 python3 script/iniGeneration/generateExampleIni.py data/object.grid.h5 2 2 2
+python3 script/fieldsInitialisation/uniformFields.py 32 32 32 data \
+'{"rhoNeutral": 5.8977e9, "v0Neutral": 7800, "v1Neutral": 0, "v2Neutral": 0, "vthNeutral": 2873}'
 echo Running PINC
 mpirun -np 8 pinc input.ini > out.log 2> err.log
 echo simulation done
